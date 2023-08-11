7d1f1c3535ec8d1585637ea6631bcf7fd3b6ed49
Sharps Zhang <sharpszhang@qq.com>
2023-08-11 10:48:31 +0800

    初始化SpringBoot项目

---
+++ web/src/main/java/org/example/message/MessageApplication.java
@@ -0,0 +1,15 @@
+package org.example.message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhang
+ * @date 2023-08-11
+ */
+@SpringBootApplication
+public class MessageApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MessageApplication.class, args);
+    }
+}
